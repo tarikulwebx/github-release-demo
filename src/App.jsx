fea269b499bfed5bf8def5ca09c2034fbbfdd11f
Md. Tarikul Islam <tarikulwebx@gmail.com>
2024-02-16 15:15:44 +0600

    Paragraph and version added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,16 @@ import React from "react";
 const App = () => {
 	return (
 		<div className="p-4">
-			<h3 className="font-bold text-2xl">Github Project Release Demo</h3>
+			<h3 className="font-bold text-2xl mb-2">
+				Github Project Release Demo{" "}
+				<span className="text-green-500">v1.1.0</span>
+			</h3>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+				Quaerat quae eaque voluptatibus asperiores, corporis ducimus
+				rerum libero ex beatae animi placeat fuga labore, odio ab
+				dolores saepe quisquam porro laborum.
+			</p>
 		</div>
 	);
 };
