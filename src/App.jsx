de11470b6681628fd8d2202ad1ab7300a63715df
Md. Tarikul Islam <tarikulwebx@gmail.com>
2024-02-16 15:05:13 +0600

    React + Tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+	return (
+		<div className="p-4">
+			<h3 className="font-bold text-2xl">Github Project Release Demo</h3>
+		</div>
+	);
+};
+
+export default App;
